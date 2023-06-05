b46b581e83888fcea620c03e3735b980f4dc5029
iaarav <aarav2008bansal@gmail.com>
2023-06-05 19:20:06 +0530

    commit

--- src/String_Assignment/Q39.java
+++ src/String_Assignment/Q39.java
@@ -7,5 +7,24 @@ public class Q39 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String: ");
         String s = sc.nextLine();
+
+        s = s + " ";
+        char ch;
+        String s1 = s.toUpperCase(), q = "";
+        String r = "" ;
+
+        for (int i = 0; i < s1.length(); i++) {
+            ch = s1.charAt(i);
+
+            if (ch == ' ') {
+                r=r+q.charAt(0);
+                q="";
+            } else {
+                q = q + ch;
+            }
+        }
+
+        System.out.println("Original String: "+s);
+        System.out.println("Required String: "+r);
     }
 }
