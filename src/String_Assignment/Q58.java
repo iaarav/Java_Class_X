79e52d8e0221dadcae7dfd7d63a7f74aa9cfe049
iaarav <aarav2008bansal@gmail.com>
2023-06-13 16:40:52 +0530

    commit

---
+++ src/String_Assignment/Q58.java
@@ -0,0 +1,11 @@
+package String_Assignment;
+
+import java.util.Scanner;
+
+public class Q58 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = sc.nextLine();
+    }
+}
---
+++ src/String_Assignment/Q72.java
@@ -0,0 +1,11 @@
+package String_Assignment;
+
+import java.util.Scanner;
+
+public class Q72 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = sc.nextLine();
+    }
+}
